036232acb29de19cb663299f8647ffa4b6763440
mauricioferrer1 <mauricio@ferrer1.com>
2022-02-06 11:56:35 -0500

    subo estructura carpertas database y configuracion sequelize e n la carpeta src

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,25 +1,25 @@
-module.exports = {
+module.exports =
 
+{
   "development": {
     "username": "root",
     "password": null,
-    "database": "shoesDB",
+    "database": "database_development",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "shoesDB"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "shoesDB"
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "shoesDB"
   }
 }
-

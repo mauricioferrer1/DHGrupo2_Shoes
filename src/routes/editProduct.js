99aca36d51913a063ac1d5a3191bc1563ed6ca94
Gaby Manes <gabymanes95@gmail.com>
2022-03-01 20:59:13 -0300

    principio validaciones

--- src/routes/editProduct.js
+++ src/routes/editProduct.js
@@ -30,3 +30,31 @@ router.get('/:id/editproduct/', controller.editProduct);
 router.put('/:id/saveproduct',upload.any('img'), controller.ProcessEditProduct);
 
 module.exports = router;
+
+
+/*
+
+const {check} = require ("express-validator")
+const validaciones = [
+    check("name")
+    .notEmpy()
+    .isLength({min:5}).withMessage("debe tener al menos 5 caracteres")
+    
+    
+    check("description")
+    .notEmpy()
+    .isLength({min:20}).withMessage("debe tener al menos 20 caracteres")
+
+
+    check("img")
+
+
+    
+    check("precio?")
+    check("category?")
+    
+]
+
+
+
+*/ 
